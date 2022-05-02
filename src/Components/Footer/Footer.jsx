2aafa32d6d2290a8a7af63119e39a36b154bcc5b
MrBekzod0899 <qurbonovbekzod1399@gmail.com>
2022-05-02 16:26:24 +0500

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,35 +1,85 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import './footer.scss'
+import logo from '../../Assets/logo3.png'
 const Footer = () => {
   return (
-    <div className="footer container-fluid">
-      <div className="row">
-        <div className="col-lg-6 col-md-6 col-sm-6 col-12">
-          <Link to="/" className="navbar__brand">
-            <img
-              src="https://sp-ao.shortpixel.ai/client/to_auto,q_lossless,ret_img/https://healthy-body.ca/wp-content/uploads/2019/04/healthy-body-digital-over-light-logo-4.png"
-              alt="logo"
-            />
-          </Link>
+    <div className="footer">
+            <div className="container">
+        <div className="row">
+            <div className="col-lg-6 col-md-6 col-sm-6 col-12">
+            <Link to="/" className="navbar__brand">
+                <img
+                src={logo}
+                alt="logo"
+                />
+            </Link>
+            <ul>
+                <li><span>1314 Upper Wentworth St,Hamilton, ON L9A 4W2</span></li>
+                <li><label htmlFor="tel">Tel:</label><a href="#!">+998337341399</a></li>
+                <li><label htmlFor="email">Email:</label><a href="#!">+998337341399</a></li>
+                <li><label htmlFor="fax">Fax:</label><a href="#!">+998337341399</a></li>
+            </ul>
+            </div>
+            <div className="col-lg-3 col-md-3 col-sm-3 col-12">
+                  <ul className="footer-list">
+                      <li>
+                          <Link to="/">Home</Link>
+                      </li>
+                      <li>
+                          <Link to="/">Meet The Team</Link>
+                      </li>
+                      <li>
+                          <Link to="/">Insurance Billing</Link>
+                      </li>
+                      <li>
+                          <Link to="/">News</Link>
+                      </li>
+                      <li>
+                          <Link to="/">Book Online</Link>
+                      </li>
+                      <li>
+                          <Link to="/">Contact Us</Link>
+                      </li>
+                  </ul>
+            </div>
+            <div className="col-lg-3 col-md-3 col-sm-3 col-12">
+                  <ul>
+                      <li>
+                          <Link to="/">Our Services</Link>
+                      </li>
+                      <li>
+                          <Link to="/">Massage Therapy</Link>
+                      </li>
+                      <li>
+                          <Link to="/">Chiropractic</Link>
+                      </li>
+                      <li>
+                          <Link to="/">Naturopathy</Link>
+                      </li>
+                      <li>
+                          <Link to="/">Physiotherapy</Link>
+                      </li>
+                      <li>
+                          <Link to="/">Acupuncture</Link>
+                      </li>
+                      <li>
+                          <Link to="/">Shockwave Therapy</Link>
+                      </li>
+                      <li>
+                          <Link to="/">Custom Orthotics</Link>
+                      </li>
+                      <li>
+                          <Link to="/">Concussion Management</Link>
+                      </li>
+                      
+                  </ul>
+            </div>
+            
         </div>
-        <div className="col-lg-3 col-md-3 col-sm-3 col-12">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam,
-          adipisci dignissimos assumenda velit facere dolorum illo
-          necessitatibus consectetur beatae! Consequatur repellat facilis eius
-          odit vero rem, atque vitae fugiat expedita maxime est temporibus
-          cupiditate perspiciatis et aliquid, error eaque voluptatibus facere
-          architecto inventore recusandae commodi! Quibusdam voluptate doloribus
-          amet nam repellendus velit assumenda nobis illo officiis sequi ducimus
-          enim, eligendi labore similique rerum impedit hic saepe libero
-          cupiditate natus? Fugiat libero in autem nam. Hic sint optio commodi
-          laborum pariatur tempora officiis iure doloribus magni ipsam. Dolorem
-          modi pariatur dicta? Magni incidunt autem sapiente, ipsa
-          exercitationem aspernatur repudiandae. Possimus, voluptates.
         </div>
-        <div className="col-lg-3 col-md-3 col-sm-3 col-12"></div>
-      </div>
     </div>
+    
   );
 };
 
