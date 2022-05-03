3a04b17afc617657d837844f7234dfc6a34ef405
MrBekzod0899 <qurbonovbekzod1399@gmail.com>
2022-05-03 12:04:01 +0500

    navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './Components/Footer';
 import Navbar from './Components/Navbar'
 import Home from './pages/Home/Home';
+import Product from './pages/Product';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
           <Navbar/>
           <Routes>
             <Route path='/' element={<Home/>}/>
+            <Route path='/product' element={<Product/>}></Route>
           </Routes>
           <Footer/>
     </BrowserRouter>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,37 +1,76 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './navbar.scss'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./navbar.scss";
+import logo from '../../Assets/logo3.png'
 
-const Navbar=()=>{
-    return (
-             <div className="container-fluid navbar">
-                    <div className="row ">
-                    <div className="col-xl-10  col-lg-12  col-md-12 col-sm-12">   
-                        <Link to='/' className='navbar__brand'>
-                            <img src='https://sp-ao.shortpixel.ai/client/to_auto,q_lossless,ret_img/https://healthy-body.ca/wp-content/uploads/2019/04/healthy-body-digital-over-light-logo-4.png' alt="logo"/>
-                        </Link>
-                        <ul className="navbar__list">
-                            <li className="navbar__item"><a href="#!">Bosh Sahifa</a></li>
-                            <li className="navbar__item"><a href="#!">Bizning Xizmat</a></li>
-                            <li className="navbar__item"><a href="#!">Mahsulotlarimiz</a>
-                                <ul className="inneritem">
-                                    <li><a href="#!">Dorilar</a></li>
-                                    <li><a href="#!">Dorilar</a></li>
-                                    <li><a href="#!">Dorilar</a></li>
-                                    <li><a href="#!">Dorilar</a></li>
-                                </ul>
-                            </li>
-                            <li className="navbar__item"><a href="#!">Home</a></li>
-                            <li className="navbar__item"><a href="#!">Home</a></li>
-                            <li className="navbar__item"><a href="#!">Home</a></li>
-                            <li className="navbar__item"><a href="#!">Home</a></li>
-                            <li className="navbar__item"><a href="#!">Home</a></li>
-                            <li className="navbar__item"><a href="#!">Home</a></li>
-                        </ul>
-                    </div>
-                    </div>     
+const Navbar = () => {
+    const [show,setShow]=useState(false)
+    console.log(show)
+    document.querySelectorAll('.navbar__item').forEach(item=>{
+        item.addEventListener('click',()=>{
+            setShow(false)
+        })
+    })
+  return (
+    <div className={`container-fluid navbar ${show ? "showNav" : ''}`}>
+      <div className="row ">
+        <div className="col-xl-10  col-lg-12  col-md-12 col-sm-12">
+        {
+            !show ? <i className="fa-solid fa-bars" onClick={()=>setShow(!show)} ></i> :
+            <i onClick={()=>setShow(!show)} className="fa-solid fa-xmark"></i>
+        }
+          <Link to="/" className="navbar__brand">
+            <img
+              src={logo}
+              alt="logo"
+            />
+          </Link>
+          <ul className="navbar__list">
+            <li className="navbar__item">
+              <Link to='/'>Bosh Sahifa</Link>
+            </li>
+            <li className="navbar__item">
+              <a href="#!">Bizning Xizmat</a>
+            </li>
+            <li className="navbar__item">
+              <Link to="/product">Mahsulotlarimiz</Link>
+              <ul className="inneritem">
+                <li>
+                  <a href="#!">Dorilar</a>
+                </li>
+                <li>
+                  <a href="#!">Dorilar</a>
+                </li>
+                <li>
+                  <a href="#!">Dorilar</a>
+                </li>
+                <li>
+                  <a href="#!">Dorilar</a>
+                </li>
+              </ul>
+            </li>
+            <li className="navbar__item">
+              <a href="#!">Home</a>
+            </li>
+            <li className="navbar__item">
+              <a href="#!">Home</a>
+            </li>
+            <li className="navbar__item">
+              <a href="#!">Home</a>
+            </li>
+            <li className="navbar__item">
+              <a href="#!">Home</a>
+            </li>
+            <li className="navbar__item">
+              <a href="#!">Home</a>
+            </li>
+            <li className="navbar__item">
+              <a href="#!">Home</a>
+            </li>
+          </ul>
         </div>
-       
-    )
-}
-export default Navbar
+      </div>
+    </div>
+  );
+};
+export default Navbar;
