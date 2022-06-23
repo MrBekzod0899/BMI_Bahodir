a2908f57feb7e5020c1d3d428858d315164a5268
MrBekzod0899 <qurbonovbekzod1399@gmail.com>
2022-06-23 21:44:37 +0500

    sog'lom tan

--- src/pages/Home/Slider/Slider.jsx
+++ src/pages/Home/Slider/Slider.jsx
@@ -11,55 +11,56 @@ const Slider = () => {
   return (
     <Splide
       options={{
-        autoplay:true,
-        arrows:false,
-        type:'loop',
+        autoplay: true,
+        arrows: false,
+        type: "loop",
       }}
       className="slider"
     >
-      <SplideSlide >
-        <div id="splide1" className="slider-content">
-        </div>
-        <div className="slider-text">
-            <div className="slider-title">
-              <h1>Care To Feel Better </h1>
-            </div>
-            <div className="slider-info">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-              esse optio tempore totam vitae autem repudiandae quasi doloribus
-              minus recusandae.
-            </div>
-            <button className="btn text-light">book online</button>
-          </div>
-      </SplideSlide>
-      <SplideSlide >
-      <div id='splide2' className="slider-content">
-      </div>
-      <div className="slider-text">
-          <div className="slider-title">
-            <h1>Care To Feel Better </h1>
-          </div>
-          <div className="slider-info">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-            esse optio tempore totam vitae autem repudiandae quasi doloribus
-            minus recusandae.
-          </div>
-          <button className="btn text-light">book online</button>
-        </div>
-      </SplideSlide>
-      <SplideSlide >
-        <div id="splide3" className="slider-content">
-        </div>
+      <SplideSlide>
+        <div id="splide1" className="slider-content"></div>
         <div className="slider-text">
           <div className="slider-title">
-            <h1>Care To Feel Better </h1>
+            <h1>Sog'ligizni asrang</h1>
           </div>
           <div className="slider-info">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-            esse optio tempore totam vitae autem repudiandae quasi doloribus
-            minus recusandae.
+            Jismoniy faollikni kundalik sog'lom turmush sayohatingizning bir
+            qismiga aylantirish uchun mashq qilishni boshlash, samarali mashq
+            qilish, yoga mashg'ulotlari va boshqalar haqida ishonchli ma'lumot
+            oling.
           </div>
-          <button className="btn text-light">book online</button>
+          <button className="btn text-light">Batafsil</button>
+        </div>
+      </SplideSlide>
+      <SplideSlide>
+        <div id="splide2" className="slider-content"></div>
+        <div className="slider-text">
+          <div className="slider-title">
+            <h1>Yoga-Sog'lik garovi</h1>
+          </div>
+          <div className="slider-info">
+            Yoga - bu yillar davomida gullab-yashnagan mashg'ulotdir; Yoga inson
+            salomatligiga juda foydali bo'lib,sizni ham jismoniy ham ruhiy
+            jihatdan chiniqtiradi. Bir soat yoga bilan shug'ullanish sizni kun
+            davomida tetik tutadi va kayfiyatingizni ko'taradi.
+          </div>
+          <button className="btn text-light">Batafsil</button>
+        </div>
+      </SplideSlide>
+      <SplideSlide>
+        <div id="splide3" className="slider-content"></div>
+        <div className="slider-text">
+          <div className="slider-title">
+            <h1>Saxsiy gigiyenaga raioya qiling</h1>
+          </div>
+          <div className="slider-info">
+            Sog’lom ovqatlanish -bu yaxshi kayfiyat va muvozanatni saqlashga
+            yordam beruvchi usul va prinsiplardir. Vazn tashlash, semirish yoki
+            kun tartibidan yaxshi o’zlashtirilmayotgan mahsulotlarni olib
+            tashlash kabi muammolar inson shaxsan tanlaydigan yo’lga bosiladigan
+            keyingi qadamdir.
+          </div>
+          <button className="btn text-light">Batafsil</button>
         </div>
       </SplideSlide>
     </Splide>
